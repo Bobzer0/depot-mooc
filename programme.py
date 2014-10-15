a82bab6487b35369e98abbb00f112443caae610f
Bobzer0 <Bobzer0@users.noreply.github.com>
2014-10-15 11:23:26 +0200

    Creation d'une calculatrice dans programme.py

--- programme.py
+++ programme.py
@@ -1,3 +1,20 @@
 # -*-coding: latin-1 -*-
 
-print("Bonjour, bienvenue dans ce programme Python")
+operateur = 0
+
+print("-- Calculatrice --\n")
+
+nombre1 = int(input("Entrez le premier nombre : "))
+nombre2 = int(input("Entrez le deuxième nombre : "))
+
+while (operateur != 1 and operateur != 2 and operateur != 3 and operateur != 4):
+	operateur = int(input("Choisissez l'opération a effectuer :\n1 = addition\n2 = soustraction\n3 = multiplication\n4 = division\n"))
+
+if operateur == 1:
+	print(nombre1, '+', nombre2, '=', nombre1 + nombre2)
+elif operateur == 2:
+	print(nombre1, '-', nombre2, '=', nombre1 - nombre2)
+elif operateur == 3:
+	print(nombre1, '*', nombre2, '=', nombre1 * nombre2)
+else:
+	print(nombre1, '/', nombre2, '=', nombre1 / nombre2)
