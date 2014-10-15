5ea4fdcdfb308ebb577ab6d240e343c098c3074e
Bobzer0 <Bobzer0@users.noreply.github.com>
2014-10-15 11:26:23 +0200

    Ajout d'une boucle pour répéter le programme de programme.py

--- programme.py
+++ programme.py
@@ -1,20 +1,24 @@
 # -*-coding: latin-1 -*-
 
 operateur = 0
+continuer = "o"
 
 print("-- Calculatrice --\n")
 
-nombre1 = int(input("Entrez le premier nombre : "))
-nombre2 = int(input("Entrez le deuxième nombre : "))
+while continuer == "o":
+	nombre1 = int(input("Entrez le premier nombre : "))
+	nombre2 = int(input("Entrez le deuxième nombre : "))
 
-while (operateur != 1 and operateur != 2 and operateur != 3 and operateur != 4):
-	operateur = int(input("Choisissez l'opération a effectuer :\n1 = addition\n2 = soustraction\n3 = multiplication\n4 = division\n"))
+	while (operateur != 1 and operateur != 2 and operateur != 3 and operateur != 4):
+		operateur = int(input("Choisissez l'opération a effectuer :\n1 = addition\n2 = soustraction\n3 = multiplication\n4 = division\n"))
 
-if operateur == 1:
-	print(nombre1, '+', nombre2, '=', nombre1 + nombre2)
-elif operateur == 2:
-	print(nombre1, '-', nombre2, '=', nombre1 - nombre2)
-elif operateur == 3:
-	print(nombre1, '*', nombre2, '=', nombre1 * nombre2)
-else:
-	print(nombre1, '/', nombre2, '=', nombre1 / nombre2)
+	if operateur == 1:
+		print(nombre1, '+', nombre2, '=', nombre1 + nombre2)
+	elif operateur == 2:
+		print(nombre1, '-', nombre2, '=', nombre1 - nombre2)
+	elif operateur == 3:
+		print(nombre1, '*', nombre2, '=', nombre1 * nombre2)
+	else:
+		print(nombre1, '/', nombre2, '=', nombre1 / nombre2)
+
+	continuer = input("Continuer (o/n) ? ")
